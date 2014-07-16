a7e8e26721f13ae09e293f9cbab4d892e4f2e5b3
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2014-07-17 08:59:01 +0900

    add template

---
+++ tmpl.js
@@ -0,0 +1,9 @@
+'use strict';
+
+//######################################################################
+function Tmpl(member) {
+  // this['class'] = this.constructor.name;
+  this.member = member;
+}
+
+exports = module.exports = Tmpl;

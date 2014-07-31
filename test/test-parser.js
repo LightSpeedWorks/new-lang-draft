a07b7cb93d2a4119c89bce34cec5ccce7deaab2a
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2014-07-31 22:08:17 +0900

    support mocha test for Parser

--- test/test-parser.js
+++ test/test-parser.js
@@ -1,58 +1,104 @@
 'use strict';
 
-describe('Parser test', function () {
-it('no errors', function (done) {
-
+var assert = require('assert');
 var util = require('util');
 var fs = require('fs');
-var path = require('path');
 
 var StringReader = require('../lib/string-reader');
 var Lexer = require('../lib/lexer');
 var Parser = require('../lib/parser');
 
-var args = process.argv.slice();
-args.shift();
-args.shift();
-
-var fileName = args.shift();
-
-fileName = fileName || path.resolve(__dirname, 'test-parser.nl'); // *** DEFAULT FILE NAME FOR TEST
-//console.log(util.inspect(args, {colors:true}));
-
-// ファイルを読んで
-// 字句解析 lexer string reader -> token
-// 構文解析(パーサ) parser token -> AST abstract syntax tree
-// 解析木を実行する run AST
-// FileReader
-
-fs.readFile(fileName, function (err, contents) {
-  if (err) {
-    console.log(err.toString());
-    process.exit(1); // error abnormal exit
-  }
-
-  var reader = new StringReader(contents.toString(), fileName);
+function test(contents, expStr, expRun) {
+  var reader = new StringReader(contents.toString(), 'fileName');
   var lexer = new Lexer(reader);
-  var parser = parser = new Parser(lexer);
+  var parser = new Parser(lexer);
   var syntax;
-  for (;;) {
-    syntax = parser.parseStatement();
-    if (syntax === null && lexer.peek() !== null) {
-      var token = lexer.read();
-      console.log('\x1b[31;1m###### ' + token + '\x1b[m');
-      console.log('### -> ' + util.inspect(token, {colors: true, depth: null}));
-      console.log();
-      continue;
-    }
-    if (syntax === null) break;
-    console.log(util.inspect(syntax, {colors: true, depth: null}));
-    console.log('###### \x1b[36;1m' + syntax + '\x1b[m');
-    console.log('### -> ' + util.inspect(syntax.run(null), {colors: true, depth: null}));
-    console.log();
+  var actStr = [];
+  var actRun = [];
+  while (syntax = parser.parseStatement()) {
+    actStr.push(syntax.toString().trim());
+    actRun.push((syntax.run() + '').trim());
   }
-  done();
-});
+  assert.deepEqual(actStr, expStr);
+  assert.deepEqual(actRun, expRun);
+}
 
-});
+var testCases = [
+  ['1;', ['1;'], ['1']],
+  ['1', ['1;'], ['1']],
+  ['1+2*3;', ['1+2*3;'], ['7']],
+  ['1?"aaa":3;', ['1?"aaa":3;'], ['aaa']],
+  ['1?\'aaa\':3;', ['1?\'aaa\':3;'], ['aaa']],
+  ['1||2;', ['1||2;'], ['1']],
+  ['1&&2;', ['1&&2;'], ['2']],
+  ['1|4;', ['1|4;'], ['5']],
+  ['5^7;', ['5^7;'], ['2']],
+  ['5&6;', ['5&6;'], ['4']],
+  ['1==2;', ['1==2;'], ['false']],
+  ['1==1;', ['1==1;'], ['true']],
+  ['1=="2";', ['1=="2";'], ['false']],
+  ['1=="1";', ['1=="1";'], ['true']],
+  ['1===2;', ['1===2;'], ['false']],
+  ['1===1;', ['1===1;'], ['true']],
+  ['1==="2";', ['1==="2";'], ['false']],
+  ['1==="1";', ['1==="1";'], ['false']],
+  ['1!=2;', ['1!=2;'], ['true']],
+  ['1!=1;', ['1!=1;'], ['false']],
+  ['1!="2";', ['1!="2";'], ['true']],
+  ['1!="1";', ['1!="1";'], ['false']],
+  ['1!==2;', ['1!==2;'], ['true']],
+  ['1!==1;', ['1!==1;'], ['false']],
+  ['1!=="2";', ['1!=="2";'], ['true']],
+  ['1!=="1";', ['1!=="1";'], ['true']],
+  ['1>2;', ['1>2;'], ['false']],
+  ['2>1;', ['2>1;'], ['true']],
+  ['2>2;', ['2>2;'], ['false']],
+  ['1<2;', ['1<2;'], ['true']],
+  ['2<1;', ['2<1;'], ['false']],
+  ['2<2;', ['2<2;'], ['false']],
+  ['1>=2;', ['1>=2;'], ['false']],
+  ['2>=1;', ['2>=1;'], ['true']],
+  ['2>=2;', ['2>=2;'], ['true']],
+  ['1<=2;', ['1<=2;'], ['true']],
+  ['2<=1;', ['2<=1;'], ['false']],
+  ['2<=2;', ['2<=2;'], ['true']],
+  ['1<<3;', ['1<<3;'], ['8']],
+  ['1024>>5;', ['1024>>5;'], ['32']],
+  ['-1>>>24;', ['-1>>>24;'], ['255']],
+  ['+2;', ['+2;'], ['2']],
+  ['-3;', ['-3;'], ['-3']],
+  ['!1;', ['!1;'], ['false']],
+  ['!0;', ['!0;'], ['true']],
+  ['!!0;', ['!!0;'], ['false']],
+  ['!!1;', ['!!1;'], ['true']],
+  ['~1;', ['~1;'], ['-2']],
+  ['~0;', ['~0;'], ['-1']],
+  ['~~0;', ['~~0;'], ['0']],
+  ['~~1;', ['~~1;'], ['1']],
+  ['typeof 123;', ['typeof 123;'], ['number']],
+  ['typeof "abc";', ['typeof "abc";'], ['string']],
+  ['null;', ['null;'], ['null']],
+  ['undefined;', ['undefined;'], ['undefined']],
+  ['true;', ['true;'], ['true']],
+  ['false;', ['false;'], ['false']],
+  ['2*((2+3));', ['2*(2+3);'], ['10']],
+  ['(1,2,3);', ['1, 2, 3;'], ['3']],
+  ['((1,2),3);', ['(1, 2), 3;'], ['3']],
+  ['(((1),2),3);', ['(1, 2), 3;'], ['3']],
+  ['(((1,2),(3,5)),4,(1,2));', ['((1, 2), (3, 5)), 4, (1, 2);'], ['2']],
+  [';', [';'], ['undefined']],
+  ['{}', ['{}'], ['undefined']],
+  ['{1}', ['{1; }'], ['1']],
+  ['{1;2}', ['{1; 2; }'], ['2']],
+  ['{;}', ['{; }'], ['undefined']],
+  ['{{}}', ['{{}}'], ['undefined']],
+  ['{};', ['{}', ';'], ['undefined', 'undefined']],
+];
+
+describe('Parser test', function () {
+  testCases.forEach(function (elem) {
+    it(elem[0].trim() + ' → ' + elem[1].join(' ') + ' → ' + elem[2].join(' '), function () {
+      test(elem[0], elem[1], elem[2]);
+    });
+  });
 });
